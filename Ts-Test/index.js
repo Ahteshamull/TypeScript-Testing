ca88f748747455cec8220374a0d684f9073b96a5
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-08-10 03:27:45 +0600

    types ready

--- Ts-Test/index.js
+++ Ts-Test/index.js
@@ -2,4 +2,4 @@ function addNumber(num1, num2) {
     console.log(num1 + num2);
 }
 addNumber(1, 2);
-addNumber(3, 4);
+addNumber(3, 200);
--- Ts-Test/index.ts
+++ Ts-Test/index.ts
@@ -1,5 +1,5 @@
-function addNumber(num1, num2) {
+function addNumber(num1 : number, num2 : number) {
     console.log(num1 + num2)
 }
 addNumber(1, 2)
-addNumber(3, 4)
+addNumber(3, 200)
