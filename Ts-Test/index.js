e1320f248cef22cb07206d340b6fd68804f27a3d
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-08-10 03:06:51 +0600

    compiler add

---
+++ Ts-Test/index.js
@@ -0,0 +1,4 @@
+var userName = 'Hasan';
+var userAge = 30;
+console.log(userName);
+console.log(userAge);
---
+++ Ts-Test/index.ts
@@ -0,0 +1,4 @@
+const userName = 'Hasan';
+const userAge = 30;
+console.log(userName);
+console.log(userAge);
