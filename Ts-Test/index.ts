ac0de6b437e8623934c920e0bc34ff075f0bbcaa
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-08-12 05:44:43 +0600

    dsfsd

--- Ts-Test/index.ts
+++ Ts-Test/index.ts
@@ -4,3 +4,6 @@ function addNumber(num10 : 20, num30 : 30) {
 export function addNumbers(num1: number, num2: number): number {
     return num1 + num2;
 }
+function addNumber22(num1 : 20, num3 : 30) {
+    console.log(num1 + num3 + 100); 
+}
