a883ad65151f749b782f3cecf7dc98a81a80aa5d
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-08-13 00:57:37 +0600

    ffff

--- Ts-Test/index.ts
+++ Ts-Test/index.ts
@@ -1,6 +1,4 @@
 function addNumber(num10 : 20, num30 : 30) {
     console.log(num10 + num30 + 100);
 }
-export function addNumbers(num1: number, num2: number): number {
-    return num1 + num2;
-}
+
